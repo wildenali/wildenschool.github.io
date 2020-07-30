4539cc8dbc6d2648716e2dcf94567a2659d1a336
wildenali <wildenwildenaliali@gmail.com>
2020-07-30 16:56:20 +0700

    Menyalin isi folder build

---
+++ precache-manifest.2d581735fb9004a8ca30a25e6ca7b777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81a79f6bd2639ac2dc25ab39b16fd23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "838a4ccaa646419974ee",
+    "url": "/static/css/main.67980736.chunk.css"
+  },
+  {
+    "revision": "f3a3f05018d79b0fa8ad",
+    "url": "/static/js/2.33376fa3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.33376fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838a4ccaa646419974ee",
+    "url": "/static/js/main.b72facc9.chunk.js"
+  },
+  {
+    "revision": "652d1a501e405e0174f8",
+    "url": "/static/js/runtime-main.47bfc52d.js"
+  }
+]);
